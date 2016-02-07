1db3b0ca1bfdc3e8ef8da4edce7caf0dd0218a9a
Tom Mornini <tom@subledger.com>
2016-02-07 11:28:04 -0800

    simplify main

--- quad-tile.go
+++ quad-tile.go
@@ -3,38 +3,10 @@ package main
 import "github.com/tmornini/quad-tile-go/position"
 import "github.com/tmornini/quad-tile-go/quad-tile"
 
-const iterations = 7500000
-
-// func main() {
-// 	for i := 0; i < iterations; i++ {
-// 		quadTile.New(position.Random(), 16)
-// 	}
-// }
-
-type quadTileChannel chan *quadTile.QuadTile
-
-const workers = 8
-
-func start_worker(channel quadTileChannel, count int) {
-	for i := 0; i < count; i++ {
-		channel <- quadTile.New(position.Random(), 16)
-	}
-}
-
-func start_workers() (quadTileChannel, int) {
-	channel := make(chan *quadTile.QuadTile)
-
-	for i := 0; i < workers; i++ {
-		go start_worker(channel, iterations/workers)
-	}
-
-	return channel, iterations
-}
+const iterations = 10000000
 
 func main() {
-	channel, iterations := start_workers()
-
 	for i := 0; i < iterations; i++ {
-		<- channel
+		quadTile.New(position.Random(), 16).IdAsString(16)
 	}
 }
