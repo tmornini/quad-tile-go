c5a5ff4895861891e5c174d84ebf067d744ead9e
Tom Mornini <tom@subledger.com>
2016-02-06 21:36:18 -0800

    even faster without recursion

--- quad-tile/id_for.go
+++ quad-tile/id_for.go
@@ -3,7 +3,7 @@ package quadTile
 import "github.com/tmornini/quad-tile-go/position"
 
 func IdFor(
-	poi           *position.Position,
+	poi *position.Position,
 	desired_level uint,
 ) string {
 	return recurse(
@@ -20,47 +20,40 @@ func IdFor(
 }
 
 func recurse(
-	id                   string,
-	latitude             float64,
-	longitude            float64,
-	center_latitude      float64,
-	center_longitude     float64,
-	desired_level        uint,
-	current_level        uint,
-	latitude_adjustment  float64,
+	id string,
+	latitude float64,
+	longitude float64,
+	center_latitude float64,
+	center_longitude float64,
+	desired_level uint,
+	current_level uint,
+	latitude_adjustment float64,
 	longitude_adjustment float64,
 ) string {
-	quadrant := 97
+	for current_level <= desired_level {
+		quadrant := 97
 
-	if longitude < center_longitude { // west
-		center_longitude -= longitude_adjustment
-	} else {                          // east
-		quadrant += 1
-		center_longitude += longitude_adjustment
+		if longitude < center_longitude { // west
+			center_longitude -= longitude_adjustment
+		} else { // east
+			quadrant++
+			center_longitude += longitude_adjustment
+		}
+
+		if latitude >= center_latitude { // north
+			center_latitude += latitude_adjustment
+		} else { // south
+			quadrant += 2
+			center_latitude -= latitude_adjustment
+		}
+
+		id += string(quadrant)
+
+		latitude_adjustment /= 2
+		longitude_adjustment /= 2
+
+		current_level++
 	}
 
-	if latitude >= center_latitude {  // north
-		center_latitude += latitude_adjustment
-	} else {                          // south
-		quadrant += 2
-		center_latitude -= latitude_adjustment
-	}
-
-	id += string(quadrant)
-
-	if current_level == desired_level {
-		return id
-	} else {
-		return recurse(
-			id,
-			latitude,
-			longitude,
-			center_latitude,
-			center_longitude,
-			desired_level,
-			current_level+1,
-			latitude_adjustment/2,
-			longitude_adjustment/2,
-		)
-	}
+	return id
 }
